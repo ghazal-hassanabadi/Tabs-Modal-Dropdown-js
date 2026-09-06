d27babff761c147a1ab3edb7c29f536e87869a28
Ghazal <hassanabadighazal85@gmail.com>
2026-09-06 15:26:00 +0330

    add tab functionality

--- js/index.js
+++ js/index.js
@@ -4,5 +4,41 @@ const allTabButtons = document.querySelectorAll(".tablinks");
 
 const tabSection = document.querySelectorAll("#tab1, #tab2, #tab3");
 
+  tabContainer.addEventListener("click", function(e) {
+
+
+    if (!e.target.classList.contains("tablinks")) {
+    return;
+}
+
+    
+        
+        const tabId = e.target.getAttribute("data-tab");
+
+
+        allTabButtons.forEach(function(button) {
+          button.classList.remove("active");
+
+        });
+
+        e.target.classList.add("active");
+
+        tabSection.forEach(function(section) {
+          section.classList.remove("active");
+          
+
+        });
+
+     
+
+        const selectedTab = document.getElementById(tabId);
+
+
+
+        selectedTab.classList.add("active");
+
+});
+
+
 
 
