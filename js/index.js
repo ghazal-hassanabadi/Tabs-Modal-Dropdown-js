90e8d61e864e22bdd4008bfc6318e55325f2fc6a
Ghazal <hassanabadighazal85@gmail.com>
2026-09-06 15:24:03 +0330

    add tab variable

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const tabContainer = document.querySelector(".tab");
+
+const allTabButtons = document.querySelectorAll(".tablinks");
+
+const tabSection = document.querySelectorAll("#tab1, #tab2, #tab3");
+
+
+
