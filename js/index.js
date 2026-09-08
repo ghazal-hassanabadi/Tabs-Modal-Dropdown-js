bdf5345508b02fbaac46ac2de56ba4bb3fe43dd5
Ghazal <hassanabadighazal85@gmail.com>
2026-09-08 15:56:24 +0330

    change css

--- js/index.js
+++ js/index.js
@@ -97,7 +97,159 @@ document.addEventListener ("keydown" ,
   }
 )
 
+/*sign */
 
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("emailError");
+
+const passwordInput = document.getElementById("password");
+const passwordError = document.getElementById("passwordError");
+
+const confirmPasswordInput = document.getElementById("confirmPassword");
+const confirmPasswordError = document.getElementById("confirmPasswordError");
+
+const form = document.getElementById("signupForm");
+
+
+
+const togglePassword = document.getElementById("togglePassword");
+const toggleConfirmPassword = document.getElementById("toggleConfirmPassword");
+
+const passwordIcon = togglePassword.querySelector("i");
+const confirmPasswordIcon = toggleConfirmPassword.querySelector("i");
+
+const successMessage = document.getElementById("successMessage");
+
+
+function isEmailValid() {
+  return emailInput.checkValidity();
+}
+
+function isPasswordValid() {
+  return passwordInput.value.length >= 6;
+}
+
+function doPasswordsMatch() {
+  return confirmPasswordInput.value === passwordInput.value;
+}
+
+emailInput.addEventListener("input",
+     function() {
+
+        if (isEmailValid())
+             {
+           emailError.textContent = "";
+             }
+
+         else {
+           emailError.textContent = "Please enter a valid email";   
+              }     
+  
+
+
+}
+
+
+
+ 
+);
+
+
+passwordInput.addEventListener("input", 
+    function() {
+
+      if (isPasswordValid())
+        {
+         passwordError.textContent = "";
+        }
+     else {
+         passwordError.textContent = "Password must be at least 6 characters";
+          }
+
+}
+
+);
+
+
+confirmPasswordInput.addEventListener("input", 
+    function() {
+
+      if (doPasswordsMatch())
+        {
+          confirmPasswordError.textContent = "";
+        } 
+      else {
+         confirmPasswordError.textContent = "Passwords do not match";
+           }
+
+}
+
+);
+
+
+form.addEventListener("submit", 
+    function(event) {
+       event.preventDefault();
+
+       if (isEmailValid() && isPasswordValid() && doPasswordsMatch())
+
+          {
+            successMessage.textContent = "ثبت نام موفق بود";
+          
+        }
+         else {
+          console.log("لطفاً فیلدها را درست پر کنید");
+        }
+}
+
+);
+
+
+
+
+
+
+
+
+
+togglePassword.addEventListener("click", 
+    function() {
+
+  if (passwordInput.type === "password") 
+    {
+       passwordInput.type = "text";
+       passwordIcon.classList.remove("fa-eye");
+       passwordIcon.classList.add("fa-eye-slash");
+  }
+   else {
+       passwordInput.type = "password";
+       passwordIcon.classList.remove("fa-eye-slash");
+       passwordIcon.classList.add("fa-eye");
+  }
+
+}
+
+);
+
+
+toggleConfirmPassword.addEventListener("click", 
+    function() {
+
+  if (confirmPasswordInput.type === "password")
+     {
+       confirmPasswordInput.type = "text";
+       confirmPasswordIcon.classList.remove("fa-eye");
+       confirmPasswordIcon.classList.add("fa-eye-slash");
+     }
+   else {
+        confirmPasswordInput.type = "password";
+        confirmPasswordIcon.classList.remove("fa-eye-slash");
+        confirmPasswordIcon.classList.add("fa-eye");
+        }
+
+}
+
+);
 
 
 /*-------------------Dropdown---------------*/
